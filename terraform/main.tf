3c246d93513c8b3d76380100597b560fe642e51b
Artem Syromiatnikov <5065091+ArtemSyromiatnikov@users.noreply.github.com>
2021-09-25 14:35:42 +0200

    Bootstrapping Terraform

---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = ">=2.78"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name   = "ping-pong-infrastructure"
+    storage_account_name  = "sapingpongterraform"
+    container_name        = "terraform-state"
+    key                   = "prod.terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
