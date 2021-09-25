f74c11e9cceff93ba2646222d9f2f9d583f8d40f
Artem Syromiatnikov <5065091+ArtemSyromiatnikov@users.noreply.github.com>
2021-09-25 16:02:19 +0200

    Terraform DBs

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,3 +21,22 @@ resource "azurerm_resource_group" "ping-pong" {
   name      = "ping-pong"
   location  = "North Europe"
 }
+
+## DB Server configuration is commented out until we decide how to deal with sensitive data
+# resource "azurerm_mssql_server" "dbs-pingpong" {
+#   name                = "dbs-pingpong"
+#   resource_group_name = azurerm_resource_group.ping-pong.name
+#   location            = azurerm_resource_group.ping-pong.location
+#   version             = "12.0"
+#   administrator_login = "lord-admin"
+#   administrator_login_password = "xxxxxxxx"    # SENSITIVE INFO!
+
+#   minimum_tls_version = "1.2"
+# }
+
+resource "azurerm_mssql_database" "db-pingpong" {
+  name = "db-pingpong"
+  server_id = "/subscriptions/c4859b0f-5443-4e12-8915-bef7d646c730/resourceGroups/ping-pong/providers/Microsoft.Sql/servers/dbs-pingpong" # for now
+  storage_account_type = "LRS"
+}
+
