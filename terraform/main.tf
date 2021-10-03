e8efe3391f62c3a472af8315d242dd0c66e62f90
Artem Syromiatnikov <5065091+ArtemSyromiatnikov@users.noreply.github.com>
2021-10-03 15:20:42 +0200

    terraform serverless db config

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,6 +52,9 @@ resource "azurerm_mssql_database" "db-pingpong" {
   server_id = azurerm_mssql_server.dbs-pingpong.id
   storage_account_type = "LRS"
   sku_name = "GP_S_Gen5_1"
+  max_size_gb = 1
+  min_capacity = 0.5
+  auto_pause_delay_in_minutes = 60
 }
 
 
