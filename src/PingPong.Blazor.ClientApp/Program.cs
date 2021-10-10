fdb025709ee6884a64032d5a153c3d67581efffa
Artem Syromiatnikov <5065091+ArtemSyromiatnikov@users.noreply.github.com>
2021-10-10 12:58:42 +0200

    Upgrade to NET Core 3.1

--- src/PingPong.Blazor.ClientApp/Program.cs
+++ src/PingPong.Blazor.ClientApp/Program.cs
@@ -1,16 +1,32 @@
-﻿using Microsoft.AspNetCore.Blazor.Hosting;
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using PeterLeslieMorris.Blazor.Validation;
+using PingPong.Blazor.Validators;
+using PingPong.Sdk;
 
 namespace PingPong.Blazor.ClientApp
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
 
-        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
-            BlazorWebAssemblyHost.CreateDefaultBuilder()
-                .UseBlazorStartup<Startup>();
+            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<HttpClient>(s =>
+            {
+                //var client = new HttpClient() { BaseAddress = new System.Uri(Configuration["PingPongApi"]) };
+                //var client = new HttpClient() { BaseAddress = new System.Uri("https://localhost:5001/") };
+                var client = new HttpClient() { BaseAddress = new System.Uri("https://pingpong-api.azurewebsites.net/") };
+                return client;
+            });
+            builder.Services.AddScoped<IApiClient, ApiClient>();
+            builder.Services.AddFormValidation(config => config.AddFluentValidation(typeof(GameValidator).Assembly));
+
+            await builder.Build().RunAsync();
+        }
     }
 }
