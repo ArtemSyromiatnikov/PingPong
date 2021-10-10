3140baa667d57e66f2853987f0caed6bacd56a68
Artem Syromiatnikov <5065091+ArtemSyromiatnikov@users.noreply.github.com>
2021-10-10 14:07:47 +0200

    Env configurations for Blazor.WebAssembly client

--- src/PingPong.Blazor.ClientApp/Program.cs
+++ src/PingPong.Blazor.ClientApp/Program.cs
@@ -14,15 +14,8 @@ namespace PingPong.Blazor.ClientApp
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-
-            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddScoped<HttpClient>(s =>
-            {
-                //var client = new HttpClient() { BaseAddress = new System.Uri(Configuration["PingPongApi"]) };
-                //var client = new HttpClient() { BaseAddress = new System.Uri("https://localhost:5001/") };
-                var client = new HttpClient() { BaseAddress = new System.Uri("https://pingpong-api.azurewebsites.net/") };
-                return client;
-            });
+            
+            builder.Services.AddScoped(_ => new HttpClient() { BaseAddress = new System.Uri(builder.Configuration["PingPongApi"]) });
             builder.Services.AddScoped<IApiClient, ApiClient>();
             builder.Services.AddFormValidation(config => config.AddFluentValidation(typeof(GameValidator).Assembly));
 
